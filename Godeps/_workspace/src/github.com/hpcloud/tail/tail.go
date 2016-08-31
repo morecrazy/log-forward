1ea17fe35530316615ab3365d61d9b1642748155
zhanghan <zhanghan@codoon.com>
2016-08-31 21:13:47 +0800

    no message

--- Godeps/_workspace/src/github.com/hpcloud/tail/tail.go
+++ Godeps/_workspace/src/github.com/hpcloud/tail/tail.go
@@ -378,6 +378,7 @@ func (tail *Tail) waitForChanges() error {
 
 	select {
 	case <-tail.changes.Modified:
+		fmt.Println("Recieve modify changes")
 		return nil
 	case <-tail.changes.Deleted:
 		tail.changes = nil
