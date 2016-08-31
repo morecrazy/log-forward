054997b61949531cac1cee7b05266a7f7ee361a6
zhanghan <zhanghan@codoon.com>
2016-08-31 21:02:28 +0800

    no message

--- Godeps/_workspace/src/github.com/hpcloud/tail/watch/polling.go
+++ Godeps/_workspace/src/github.com/hpcloud/tail/watch/polling.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"runtime"
 	"time"
+	"fmt"
 
 	"github.com/hpcloud/tail/util"
 	"gopkg.in/tomb.v1"
@@ -58,6 +59,9 @@ func (fw *PollingFileWatcher) ChangeEvents(t *tomb.Tomb, pos int64) (*FileChange
 
 	go func() {
 		prevSize := fw.Size
+		if fw.Filename == "/var/log/go_log/ucenter.log" {
+			fmt.Printf("Pre file size is %s\n", prevSize)
+		}
 		for {
 			select {
 			case <-t.Dying():
@@ -80,6 +84,10 @@ func (fw *PollingFileWatcher) ChangeEvents(t *tomb.Tomb, pos int64) (*FileChange
 				util.Fatal("Failed to stat file %v: %v", fw.Filename, err)
 			}
 
+			if fw.Filename == "/var/log/go_log/ucenter.log" {
+				fmt.Printf("Current file size is %s\n", fw.Size)
+			}
+
 			// File got moved/renamed?
 			if !os.SameFile(origFi, fi) {
 				changes.NotifyDeleted()
@@ -96,6 +104,9 @@ func (fw *PollingFileWatcher) ChangeEvents(t *tomb.Tomb, pos int64) (*FileChange
 			// File got bigger?
 			if prevSize > 0 && prevSize < fw.Size {
 				changes.NotifyModified()
+				if fw.Filename == "/var/log/go_log/ucenter.log" {
+					fmt.Println("File become bigger")
+				}
 				prevSize = fw.Size
 				continue
 			}
